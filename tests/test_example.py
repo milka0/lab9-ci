f37d7709cb94b25e636ccc2b1a0935b85b24eef4
Kamilla <kamilla@example.com>
2025-12-31 20:03:04 +0300

    Replace tests with API tests from previous labs

--- tests/test_example.py
+++ tests/test_example.py
@@ -1,2 +1,22 @@
-def test_sum():
-    assert 2 + 2 == 4
+import requests
+
+
+def test_get_post_1_status_code():
+    response = requests.get(
+        "https://jsonplaceholder.typicode.com/posts/1",
+        timeout=10
+    )
+    assert response.status_code == 200
+
+
+def test_get_post_1_has_required_fields():
+    response = requests.get(
+        "https://jsonplaceholder.typicode.com/posts/1",
+        timeout=10
+    )
+    data = response.json()
+
+    assert "userId" in data
+    assert "id" in data
+    assert "title" in data
+    assert "body" in data
